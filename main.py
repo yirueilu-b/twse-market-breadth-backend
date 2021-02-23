f4e2b2955eeaa81754f8d8c410c7794c11d055c6
yirueilu <yirueilu.b@gmail.com>
2021-02-23 07:40:18 +0000

    Fix path on pythonanywhere

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ api = Api(app)
 
 class MarketBreadth(Resource):
     def get(self, date):
-        file_path = os.path.join(utils.DATA_DIR, utils.MARKET_BREADTH_DIR, '{}.json'.format(date))
+        file_path = os.path.join('twse-market-breadth-backend', utils.DATA_DIR, utils.MARKET_BREADTH_DIR, '{}.json'.format(date))
         if not os.path.exists(file_path):
             response = jsonify(
                 {"ERROR": "File not found, {} is not a valid date. Please try another date.".format(date)})
@@ -30,7 +30,7 @@ class MarketBreadth(Resource):
 
 class IndexInfo(Resource):
     def get(self, date):
-        index_df = pd.read_csv(glob.glob(os.path.join(utils.DATA_DIR, utils.INDEX_HISTORY_DIR, '*'))[-1])
+        index_df = pd.read_csv(glob.glob(os.path.join('twse-market-breadth-backend', utils.DATA_DIR, utils.INDEX_HISTORY_DIR, '*'))[-1])
         request_date = date
         date = '/'.join([date[:4], date[4:6], date[6:]])
         date = date.split('/')
@@ -51,7 +51,7 @@ class IndexInfo(Resource):
 
 class IndexMonth(Resource):
     def get(self):
-        index_df = pd.read_csv(glob.glob(os.path.join(utils.DATA_DIR, utils.INDEX_HISTORY_DIR, '*'))[-1])
+        index_df = pd.read_csv(glob.glob(os.path.join('twse-market-breadth-backend', utils.DATA_DIR, utils.INDEX_HISTORY_DIR, '*'))[-1])
         res = index_df.close.iloc[-20:].tolist()
         date_list = index_df.date.iloc[-20:].tolist()
         for i in range(len(date_list)):
